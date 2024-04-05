27b203619b098d958dd683e4aa4dc301d16a1653
Eric Chung <eric.chung@fdmgroup.com>
2024-04-05 11:07:32 +0800

    Use the component in home and pickup-calls

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { ComponentsModule } from 'src/app/components/components.module';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePageRoutingModule } from './home-routing.module';
@@ -12,6 +12,7 @@ import { HomePage } from './home.page';
   imports: [
     CommonModule,
     FormsModule,
+    ComponentsModule,
     IonicModule,
     HomePageRoutingModule
   ],
